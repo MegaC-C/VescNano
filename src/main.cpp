54df96e08642f7fac0602ee8751a19972cc65c8e
megaC <corloss@web.de>
2024-03-29 20:46:07 +0100

    add from 0 to 40kmh

--- src/main.cpp
+++ src/main.cpp
@@ -20,12 +20,24 @@ void loop()
     // put your main code here, to run repeatedly:
     // Serial.println("rpm = 200");
 
-    UART.setRPM(500);
+    UART.setRPM(0);
     delay(delay_ms);
 
-    UART.setRPM(1000);
+    UART.setRPM(1723);
     delay(delay_ms);
 
-    UART.setRPM(2000);
+    UART.setRPM(3446);
+    delay(delay_ms);
+
+    UART.setRPM(5169);
+    delay(delay_ms);
+
+    UART.setRPM(6893);
     delay(delay_ms);
 }
+
+// 02050800000000022D03 
+// 020508000006BBBE3B03 
+// 02050800000D766A2003 
+// 02050800001431EBE803 
+// 02050800001AEDC21603 
